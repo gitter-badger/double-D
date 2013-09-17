e0241ed21f5248f2fbd3b7a3b8084f6040a50eaa
gfspock <gfspock@gmail.com>
2013-09-17 16:56:55 +0300

    Created "Products" module.

---
+++ module/Products/config/module.config.php
@@ -0,0 +1,45 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Products\Controller\Products' => 'Products\Controller\ProductsController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'products' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/products[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Products\Controller\Products',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'products' => __DIR__ . '/../view',
+        ),
+    ),
+//    'router' => array(
+//        'routes' => array(
+//            'products'=>array(
+//                'type'=>'simple',
+//                'options'=>array(
+//                    'route'=>'/products',
+//                    'defaults'=>array(
+//                        'controller'=>'Products\Controller\Products',
+//                        'action' =>'index',
+//                    )
+//                )
+//            )
+//        )
+//    ),
+);
---
+++ module/Products/src/Products/Controller/ProductsController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Products\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ProductsController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+}
