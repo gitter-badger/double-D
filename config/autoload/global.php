e51340e5c54148aba9ac97bd89c9bd9e6034dbb7
gfspock <gfspock@gmail.com>
2014-03-15 22:36:03 +0200

    Config file.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,53 +1,21 @@
 <?php
-/**
- * Global Configuration Override
- *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
 
-/* todo: uncomment next lines on server machine.*/
-//$dsn = sprintf(
-//    'mysql:dbname=gfspock;host=gfspock-db.my.phpcloud.com',
-//    get_cfg_var('zend_developer_cloud.db.gfspock'),
-//    get_cfg_var('zend_developer_cloud.db.gfspock-db.my.phpcloud.com')
-//);
-//
-//return array(
-//    'db' => array(
-//        'driver'         => 'Pdo',
-//        'dsn'            => $dsn,
-//        'username'  => 'gfspock',
-//        'password'  => 'a025628',
-//        'driver_options' => array(
-//            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-//        ),
-//    ),
-//    'service_manager' => array(
-//        'factories' => array(
-//            'Zend\Db\Adapter\Adapter'
-//            => 'Zend\Db\Adapter\AdapterServiceFactory',
-//        ),
-//    ),
-//);
-/* todo: uncomment next lines on local machine*/
+// local machine
+//$dbase="main";
+//$host="localhost";
+
+// qa-machine:
+$dbase="dikea";
+$host="127.6.222.3";
+
 $dsn = sprintf(
-    'mysql:dbname=main;host=localhost',
-    get_cfg_var('zend_developer_cloud.db.groot'),
-    get_cfg_var('zend_developer_cloud.db.localhost')
+    'mysql:dbname='.$dbase.';host='.$host
 );
 
 return array(
     'db' => array(
         'driver'         => 'Pdo',
         'dsn'            => $dsn,
-        'username'  => 'root',
-        'password'  => '',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
