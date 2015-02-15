1e11f2be80c97e2171ececbbe52a9d0afc7c641a
Bogdan Didukh <gfspock@gmail.com>
2015-02-16 00:38:58 +0200

    Created tasks for building project.

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dikeaApp
+ * @description
+ * # dikeaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dikeaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dikeaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dikeaApp
+ */
+angular.module('dikeaApp')
+  .controller('AboutCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.makeRequest = function () {
+      $http.post('/products/requests/getProductsList').success(function (data) {
+        console.log('data is:', data);
+      });
+    };
+
+    $scope.makeRequest();
+  });
