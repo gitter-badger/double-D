886d75668173a89518652c3df63b58e78e37b3d5
Volodya Skalskyi <volodya.skalskyi@gmail.com>
2015-02-17 13:12:37 +0200

    Creating page for each products.

--- app/scripts/controllers/oneproduct.js
+++ app/scripts/controllers/oneproduct.js
@@ -8,6 +8,29 @@
  * Controller of the dikeaApp
  */
 angular.module('dikeaApp')
-  .controller('OneproductCtrl', function ($scope) {
-
+  .controller('OneproductCtrl', function ($scope,$http,$routeParams) {
+    $scope.number = 1;
+    $scope.added = "";
+    $scope.storage = new LocalStorage();
+    $scope.init = function (data) {
+      $scope.data = data;
+      $scope.getNavigation();
+    }
+    $scope.price = function () {
+      return $scope.data.price * $scope.number;
+    }
+    $scope.buy = function () {
+      if ($scope.price()) {
+        $scope.storage.addToCart($scope.data, $scope.number);
+        $scope.added = "РґРѕР±Р°РІР»РµРЅРѕ РІ РєРѕСЂР·РёРЅСѓ";
+      }
+    }
+    $scope.getNavigation = function () {
+      $http.post("/category/requests/" + $scope.data.list_type + "/" + $scope.data.list_id, {"action": "getShortNavigation"}).success(function (data) {
+        $scope.navigation = data;
+        if(data.length==0){
+          location.href="/"
+        }
+      });
+    }
   });
