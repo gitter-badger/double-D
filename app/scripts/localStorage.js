edfc5b6c1bcb3cbf3937088b3e5992aea1ec8935
Volodya Skalskyi <volodya.skalskyi@gmail.com>
2015-02-18 20:20:50 +0200

    I should fix all the errors and warnings in code, because jshint task doesn't works correctly. localStorage.js

--- app/scripts/localStorage.js
+++ app/scripts/localStorage.js
@@ -5,62 +5,62 @@ function LocalStorage() {
   var th = this;
 
   this.init = function () {
-    $("#cart_number").html(th.getNumbers());
-  }
+    $('#cart_number').html(th.getNumbers());
+  };
   this.set = function (name, value) {
     if (!th.check()) {
       return false;
     }
     localStorage.setItem(name, JSON.stringify(value));
     return true;
-  }
+  };
   this.get = function (name) {
     if (!th.check()) {
       return false;
     }
     var cart = localStorage.getItem(name);
     return JSON.parse(cart);
-  }
+  };
   this.delete = function (name) {
     if (!th.check()) {
       return false;
     }
     localStorage.removeItem(name);
-  }
+  };
   this.check = function () {
     try {
       return 'localStorage' in window && window['localStorage'] !== null;
     } catch (e) {
       return false;
     }
-  }
+  };
   this.getNumbers = function () {
-    var data = th.get("shopping_cart");
-    var number = 0
+    var data = th.get('shopping_cart');
+    var number = 0;
     $(data).each(function () {
       number += this.number;
-    })
-    $("#cart_number").html(number);
+    });
+    $('#cart_number').html(number);
     return number;
-  }
+  };
   this.addToCart = function (data, number) {
-    var cart = th.get("shopping_cart");
+    var cart = th.get('shopping_cart');
     if (!cart) {
       cart = [];
     }
     var exist = false;
     $(cart).each(function () {
-      if (this.item.id == data.id) {
+      if (this.item.id === data.id) {
         this.number += number;
         exist = true;
         return;
       }
-    })
+    });
     if (!exist) {
       cart.push({item: data, number: number});
     }
-    th.set("shopping_cart", cart);
+    th.set('shopping_cart', cart);
     th.getNumbers();
-  }
+  };
   this.init();
 }
