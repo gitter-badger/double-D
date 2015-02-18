43a388cc3e916673c56111b6e5d8defaddc6d831
Volodya Skalskyi <volodya.skalskyi@gmail.com>
2015-02-18 20:17:41 +0200

    I should fix all the errors and warnings in code, because jshint task doesn't works correctly. productslist.js

--- app/scripts/controllers/productslist.js
+++ app/scripts/controllers/productslist.js
@@ -16,48 +16,48 @@ angular.module('dikeaApp')
       $scope.list_id = $routeParams.Id;
       $scope.getProducts();
       $scope.getNavigation();
-    }
+    };
     $scope.init2 = function (type, id) {
       $scope.list_type = type;
       $scope.list_id = id;
       $scope.getProducts();
       $scope.getNavigation();
 
-    }
+    };
     $scope.getProducts = function () {
-      $http.post("/category/requests/" + $scope.list_type + "/" + $scope.list_id, {"action": "getProducts"}).success(function (data) {
+      $http.post('/category/requests/' + $scope.list_type + '/' + $scope.list_id, {'action': 'getProducts'}).success(function (data) {
         $scope.prd = data;
       });
-    }
+    };
 
     $scope.getNavigation = function () {
-      $http.post("/category/requests/" + $scope.list_type + "/" + $scope.list_id, {"action": "getNavigation"}).success(function (data) {
+      $http.post('/category/requests/' + $scope.list_type + '/' + $scope.list_id, {'action': 'getNavigation'}).success(function (data) {
         $scope.navigation_data = data;
-        if (data.length == 0) {
-          location.href = "/"
+        if (data.length === 0) {
+          location.href = '/';
         }
         $scope.header = data[0].header;
       });
-    }
+    };
     $scope.isActive = function (id) {
-      if (id == $scope.list_id) {
-        return "active";
+      if (id === $scope.list_id) {
+        return 'active';
       }
-      return "";
-    }
+      return '';
+    };
 
     $scope.buy = function (item) {
       $scope.storage.addToCart(item, 1);
       item.added = true;
-    }
+    };
     $scope.added = function (item) {
       if (item.added) {
-        $("#"+item.id+"").html("добавлено в корзину");
-        return "добавлено в корзину"
+        $('#'+item.id+'').html('добавлено в корзину');
+        return 'добавлено в корзину';
       } else {
-        $("#"+item.id+"").html("");
-        return "";
+        $('#'+item.id+'').html('');
+        return '';
       }
-    }
+    };
     $scope.init();
   });
