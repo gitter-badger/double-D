78c2dbbdfab494244a16bca011f5e7c8d31c336d
Volodya Skalskyi <volodya.skalskyi@gmail.com>
2015-02-23 19:38:16 +0200

    It's works good! ;-)

--- app/scripts/controllers/oneproduct.js
+++ app/scripts/controllers/oneproduct.js
@@ -8,30 +8,30 @@
  * Controller of the dikeaApp
  */
 angular.module('dikeaApp')//Product page Ctrl. View: 'oneproduct.html'
-  .controller('OneproductCtrl', function ($scope,$http,$routeParams) {
+  .controller('OneproductCtrl', function ($scope, $http, $routeParams) {
     $scope.number = 1;
     $scope.added = '';
     $scope.storage = new LocalStorage();
-    $scope.data={};
-    $scope.init=function(){
-      $scope.id=$routeParams.Id;
+    $scope.data = {};
+    $scope.init = function () {
+      $scope.id = $routeParams.Id;
       $scope.getData($scope.id);
     };
-    $scope.getData=function(id){
+    $scope.getData = function (id) {
       //getting data
-      $http.get('/products/requests/product?id='+id+'').success(function(data){
-        $scope.data=data.data;
-        if(!data.data){
-          location.href='/#main';
+      $http.get('/products/requests/product?id=' + id + '').success(function (data) {
+        $scope.data = data.data;
+        if ((!data.data)||(data.data.id=='')) {
+          location.href = '/#main';
         }
         //getting navigation
         $http.post("/category/requests/" + data.data.list_type + "/" + data.data.list_id, {"action": "getShortNavigation"}).success(function (data) {
           $scope.navigation = data;
-          if(data.length==0){
-            location.href="/"
+          if (data.length == 0) {
+            location.href = "/"
           }
         });
-      });
+      })
     };
     $scope.price = function () {
       return $scope.data.price * $scope.number;
