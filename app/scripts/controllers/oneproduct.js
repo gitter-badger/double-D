47902d446d79e9c5e206b1d21e4b3aef78b25657
Volodya Skalskyi <volodya.skalskyi@gmail.com>
2015-02-24 19:46:11 +0200

    Creating new stuff in product page.

--- app/scripts/controllers/oneproduct.js
+++ app/scripts/controllers/oneproduct.js
@@ -13,7 +13,8 @@ angular.module('dikeaApp')//Product page Ctrl. View: 'oneproduct.html'
     $scope.added = '';
     $scope.storage = new LocalStorage();
     $scope.data = {};
-    $scope.prd={};
+    $scope.prd1=new Array(3);
+
     $scope.init = function () {
       $scope.id = $routeParams.Id;
       $scope.getData($scope.id);
@@ -31,6 +32,12 @@ angular.module('dikeaApp')//Product page Ctrl. View: 'oneproduct.html'
           if (data.length == 0) {
             location.href = "/"
           }
+        });
+        $http.post('/category/requests/' + data.data.list_type + '/' + data.data.list_id, {'action': 'getProducts'}).success(function (data) {
+          $scope.prd = data;
+          $scope.prd1[0]=$scope.prd[0];
+          $scope.prd1[1]=$scope.prd[1];
+          $scope.prd1[2]=$scope.prd[2];
         });
       })
     };
