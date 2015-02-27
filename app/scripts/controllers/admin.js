34b12c94bb7b7d09ed19baee8f61253fc40ceb6f
Volodya Skalskyi <volodya.skalskyi@gmail.com>
2015-02-27 19:56:23 +0200

    Admin login page.

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -11,7 +11,7 @@ angular.module('dikeaApp')
   .controller('AdminCtrl', function ($scope) {
          $scope.user={};
          $scope.submitForm=function(){
-           alert('User name: '+$scope.user.userName);
+           alert('User name: '+$scope.user.userName+' Password: '+$scope.user.Password);
          };
          $scope.errorSubmit=function(){
            alert("Error!");
