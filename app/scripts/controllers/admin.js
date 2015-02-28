660197de7c4fc706c426d83b42a812e52c81066c
Volodya Skalskyi <volodya.skalskyi@gmail .com>
2015-02-28 17:17:21 +0200

    Small fixes.

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -13,9 +13,9 @@ angular.module('dikeaApp')
          $scope.localStorage=new LocalStorage();
          $scope.localStorage.getNumbers();
          $scope.submitForm=function(){
-           //alert('User name: '+$scope.user.userName+' Password: '+$scope.user.Password);
+           console.log('User name: '+$scope.user.userName+' Password: '+$scope.user.Password);
          };
          $scope.errorSubmit=function(){
-          // alert('Fill the all fields!');
+          console.log('Fill the all fields!');
          };
   });
