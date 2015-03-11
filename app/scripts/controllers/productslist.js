9e569ac360ad38a1eaaafec7e8e6e740e7f1449b
mohsom <volodya.skalskyi@gmail.com>
2015-03-11 20:15:26 +0200

    Small fixes.

--- app/scripts/controllers/productslist.js
+++ app/scripts/controllers/productslist.js
@@ -80,10 +80,10 @@ angular.module('dikeaApp')//Products in Category Ctrl. View: 'productslist.html'
     };
     $scope.disableAddedPrds=function(){
        $scope.cartPrds=$scope.storage.get('shopping_cart');
-       angular.forEach($scope.cartPrds,function(data){
-         //$('#'+data.item.id+'-prd').attr('disabled',''); //doesn't works!
-         //$('#'+data.item.id+'').html('добавлено в корзину');
-       });
+       //angular.forEach($scope.cartPrds,function(data){
+       //  //$('#'+data.item.id+'-prd').attr('disabled',''); //doesn't works!
+       //  //$('#'+data.item.id+'').html('добавлено в корзину');
+       //});
     };
     $scope.init();
   });
