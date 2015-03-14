b3d2bb36c4cb921acbeb48c545f2c0ea50c04b93
mohsom <volodya.skalskyi@gmail.com>
2015-03-14 14:24:23 +0200

    Some changes.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,35 @@
  * Controller of the dikeaApp
  */
 angular.module('dikeaApp')//App Ctrl. View: 'main.html'
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$http) {
     $('title').html('Главная |DIKEA');
     $scope.storage = new LocalStorage();
     $scope.storage.getNumbers();
+    $scope.list_type='living_room';
+    $scope.list_id=16239;
+    $scope.init=function(){
+         $scope.getProducts();
+    };
+    $scope.getProducts = function () {
+      $http.post('/category/requests/' + $scope.list_type + '/' + $scope.list_id, {'action': 'getProducts'}).success(function (data) {
+        $scope.prd = data;
+        $scope.prd=$scope.prd.slice(0,3);
+      });
+    };
+    $scope.buy = function (item) {
+      $scope.storage.addToCart(item, 1);
+      item.added = true;
+    };
+    $scope.added = function (item) {
+      if (item.added) {
+        $('#'+item.id+'').html('добавлено в корзину');
+        $('#'+item.id+'-prd').attr('disabled','');
+        return 'добавлено в корзину';
+      } else {
+        $('#'+item.id+'-prd').removeAttr('disabled');
+        $('#'+item.id+'').html('');
+        return '';
+      }
+    };
+    $scope.init();
   });
