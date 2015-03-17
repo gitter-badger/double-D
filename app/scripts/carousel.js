8454ebcf407650aad506337ae0d215a35ab00a63
mohsom <volodya.skalskyi@gmail.com>
2015-03-17 18:25:38 +0200

    Carousel.

--- app/scripts/carousel.js
+++ app/scripts/carousel.js
@@ -1,12 +1,18 @@
 /**
  * Created by mohsom on 16.03.2015.
  */
-$(document).ready(function(){
-     $('.radio-button').click(function(){
-        var id=$(this).attr('id');
-        $('.active-radio').removeClass('active-radio');
-        $('#'+id).addClass('active-radio');
-        $('.slide-active').removeClass('slide-active');
-       $('[data-slide='+id+']').addClass('slide-active');
-     });
+$(document).ready(function () {
+  $('.radio-button').click(function () {
+    var id = $(this).attr('id');
+    $('.active-radio').removeClass('active-radio');
+    $('#' + id).addClass('active-radio');
+    $('.slide-active').removeClass('slide-active');
+    $('[data-slide=' + id + ']').addClass('slide-active');
+  });
+  $('.left-arrow').click(function () {
+
+  });
+  $('.right-arrow').click(function () {
+
+  });
 });
