8c2508d98bf7631eb5a6fc10b6340e1bdb07ac9a
mohsom <volodya.skalskyi@gmail.com>
2015-03-17 17:59:36 +0200

    Carousel.

--- app/scripts/carousel.js
+++ app/scripts/carousel.js
@@ -1,9 +1,6 @@
 /**
  * Created by mohsom on 16.03.2015.
  */
-$(function(){
-     $('#radio-slide1').click(function(){
-        $('.active-radio').removeClass('active-radio');
-        $('#radio-slide1').addClass('active-radio');
-     });
+$(window).onload(function(){
+     
 });
