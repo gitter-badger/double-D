c102f2795ffaedba3c2dc222a04123836ed2d24b
mohsom <volodya.skalskyi@gmail.com>
2015-03-17 18:51:54 +0200

    Carousel.

--- app/scripts/carousel.js
+++ app/scripts/carousel.js
@@ -13,6 +13,12 @@ $(document).ready(function () {
     $('[data-slide=' + id + ']').addClass('slide-active');
   });
   $('.left-arrow').click(function () {
+    prevSlide();
+  });
+  $('.right-arrow').click(function () {
+    nextSlide();
+  });
+  var prevSlide=function(){
     currentSlide--;
     if(currentSlide<1) {
       currentSlide = 4;
@@ -21,15 +27,16 @@ $(document).ready(function () {
     $('#slide'+currentSlide).addClass('slide-active');
     $('.active-radio').removeClass('active-radio');
     $('#radio-slide'+currentSlide).addClass('active-radio');
-  });
-  $('.right-arrow').click(function () {
-      currentSlide++;
-      if(currentSlide>4) {
-        currentSlide = 1;
-      }
-      $('.slide-active').removeClass('slide-active');
-      $('#slide'+currentSlide).addClass('slide-active');
-      $('.active-radio').removeClass('active-radio');
-      $('#radio-slide'+currentSlide).addClass('active-radio');
-  });
+  };
+  var nextSlide=function(){
+    currentSlide++;
+    if(currentSlide>4) {
+      currentSlide = 1;
+    }
+    $('.slide-active').removeClass('slide-active');
+    $('#slide'+currentSlide).addClass('slide-active');
+    $('.active-radio').removeClass('active-radio');
+    $('#radio-slide'+currentSlide).addClass('active-radio');
+  };
+  setInterval(nextSlide,10000);
 });
